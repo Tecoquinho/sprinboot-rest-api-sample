024ad65922894d7ca9e1adbcf294e4898808c05c
Matheus Lage <matheus.lage@icloud.com>
2024-05-01 04:04:49 -0300

    Adicionando primeiro metodo da API

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,11 +1,20 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
 
 /**
  *
@@ -13,6 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired   /*Injeção de dependencia*/
+	private UsuarioRepository usuarioRepository;
+	
     /**
      *
      * @param name the name to greet
@@ -27,6 +40,18 @@ public class GreetingsController {
     @RequestMapping(value = "/teste/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String testeEndPoint(@PathVariable String name) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(name);
+    	usuarioRepository.save(usuario); /*Obviamente, grava no BD*/
+    	
         return "criação de Endpoint " + name + "!";
+    }
+    
+    @GetMapping(value="listatodos")
+    @ResponseBody /*Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna lista em JSON*/
     }
 }
